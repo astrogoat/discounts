de2480d42332163b66b1e8df66bd77a46bcf5a3c
Kofi Ramos <karsoft92@gmail.com>
2024-03-06 11:24:59 +0000

    DTCE-918: MSRP (Manufacturer's or supplier's Suggested Retail Price) in Discounts app (#8)
    Co-authored-by: Kofi Ramos <>

--- resources/views/settings/types/index.blade.php
+++ resources/views/settings/types/index.blade.php
@@ -11,4 +11,18 @@
     </x-fab::forms.select>
 
     @include($this->getSelectedTypeView(), ['component' => $this->getSelectedType()::getName()])
+
+    <x-fab::forms.input
+        wire:model="payload.legal_link_copy"
+        name="payload[legal_link_copy]"
+        label="Legal link copy"
+        class="mb-4 mt-4"
+    />
+
+    <x-fab::forms.editor
+        wire:model="payload.legal"
+        name="payload[legal]"
+        label="Legal copy"
+        class="mb-4 mt-4"
+    />
 </div>
